3a131176c4998f141d70b71e692bc53777d0c716
root <seth.a.yoder@gmail.com>
2015-06-17 09:48:23 -0400

    Option to use small CSS

--- setup.sh
+++ setup.sh
@@ -2,11 +2,27 @@
 
 DIR=$1
 
+cd ./assets/css
+if [ -e "./styles_large.css" ]; then
+    mv styles.css styles_small.css
+    mv styles_large.css styles.css
+    cd ../..
+fi
+
 if [ -z $DIR ]; then
     echo "Usage: ./setup.sh DIR"
     echo "       where DIR is the name of the data directory."
     echo "Example: ./setup.sh Obituaries"
     exit 1
+fi
+
+CSS=$2
+
+if [ $CSS = "--small" ]; then
+    cd ./assets/css
+    mv styles.css styles_large.css
+    mv styles_small.css styles.css
+    cd ../..
 fi
 
 cp .scan.php.default scan.php || exit 1
